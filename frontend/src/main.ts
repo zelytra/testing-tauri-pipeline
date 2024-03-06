8448ce5d9a082d659909144b3126b45c64435ec4
Zelytra <contact@zelytra.fr>
2024-03-06 19:30:17 +0100

    v0.1.1

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,6 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+    <h1>UPDATE FAITE !</h1>
   </div>
 `
 
