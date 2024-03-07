9af112ff2828ea461b72cd97fe51f6573749f457
Alexandre PETIT <alexbreuillet@gmail.com>
2024-03-07 16:15:12 +0100

    final de chez final des tests 42

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,7 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
-    <h1>UPDATE FAITE !</h1>
+    <h1>UPDATE FAITE ! LEZZZ GOOO ?</h1>
   </div>
 `
 
